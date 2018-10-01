17b1c2113200d7df382a598e3d59f01d249c3629
sdshaoda <sdshaoda@126.com>
2018-10-01 10:39:14 +0800

    remove useless

---
+++ todolist/src/TodoList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>hello</div>
+    );
+  }
+}
+
+export default TodoList;
