25fc577527b792e1633d69ba31caff5822a4d843
sdshaoda <sdshaoda@126.com>
2024-03-27 00:14:49 +0800

    optimize ssr demo like next.js

--- ssr/server.js
+++ ssr/server.js
@@ -1,26 +1,50 @@
 import express from 'express'
 import React from 'react'
 import { renderToString } from 'react-dom/server'
-import App from './pages/index'
+import { readdirSync } from 'fs'
+import { join } from 'path'
 
 const app = express()
 app.use(express.static('public'))
-const content = renderToString(<App />)
 
-app.get('/', (req, res) =>
-  res.send(`
-<html>
-   <head>
-       <title>Tiny React SSR</title>
-   </head>
-   <body>
-    <div id='root'>
-      ${content}
-    </div>
-    <script src="/client.bundle.js"></script>
-   </body>
-</html>
-`),
-)
+const pagesDir = join(process.cwd(), '/pages')
+const pages = readdirSync(pagesDir).map(page => page.split('.')[0])
+
+app.get(/.*$/, async (req, res) => {
+  const path = req.path.split('/')[1]
+  const page = path ? path : 'index'
+
+  if (pages.includes(page)) {
+    const file = await import(`./pages/${page}.js`)
+    const Component = file.default
+
+    let propsObj = {}
+    if (file.getServerSideProps) {
+      const { props } = await file.getServerSideProps({ query: req.query })
+      propsObj = props
+    }
+
+    const content = renderToString(<Component {...propsObj} />)
+    res.send(`
+      <html>
+         <head>
+             <title>Tiny React SSR</title>
+         </head>
+         <body>
+          <div id='root'>${content}</div>
+          <script>
+            window.__DATA__ = ${JSON.stringify({
+              props: propsObj,
+              page: page,
+            })}
+          </script>
+          <script src="/client.bundle.js"></script>
+         </body>
+      </html>
+      `)
+  } else {
+    return res.status(200).json({ message: `${page} not found in ${pages}` })
+  }
+})
 
 app.listen(3000, () => console.log('listening on port 3000!'))
