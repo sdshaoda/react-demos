b41c7c90171b2b3488b407923fee7c3a8914c8f3
sdshaoda <sdshaoda@126.com>
2024-04-13 01:01:17 +0800

    pref(rsc): router

--- rsc/utils.ts
+++ rsc/utils.ts
@@ -1,12 +1,16 @@
 import escapeHtml from 'escape-html'
 
-export function renderJSXToHTML(jsx) {
+export async function renderJSXToHTML(jsx) {
   if (typeof jsx === 'string' || typeof jsx === 'number') {
     return escapeHtml(jsx)
   } else if (jsx == null || typeof jsx === 'boolean') {
     return ''
   } else if (Array.isArray(jsx)) {
-    return jsx.map(child => renderJSXToHTML(child)).join('')
+    // 这里添加了 await 和 Promise.all
+    const childHtmls = await Promise.all(
+      jsx.map(child => renderJSXToHTML(child)),
+    )
+    return childHtmls.join('')
   } else if (typeof jsx === 'object') {
     if (jsx.$$typeof === Symbol.for('react.element')) {
       if (typeof jsx.type === 'string') {
@@ -21,19 +25,24 @@ export function renderJSXToHTML(jsx) {
           }
         }
         html += '>'
-        html += renderJSXToHTML(jsx.props.children)
+        // 这里添加了 await
+        html += await renderJSXToHTML(jsx.props.children)
         html += '</' + jsx.type + '>'
         html = html.replace(/className/g, 'class')
         return html
       } else if (typeof jsx.type === 'function') {
-        // 组件类型如 <BlogPostPage>
         const Component = jsx.type
         const props = jsx.props
-        const returnedJsx = Component(props)
+        // 这里添加了 await
+        const returnedJsx = await Component(props)
         return renderJSXToHTML(returnedJsx)
       } else {
         throw new Error('Not implemented.')
       }
-    } else throw new Error('Cannot render an object.')
-  } else throw new Error('Not implemented.')
+    } else {
+      throw new Error('Cannot render an object.')
+    }
+  } else {
+    throw new Error('Not implemented.')
+  }
 }
