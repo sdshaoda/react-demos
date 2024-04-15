f46a8590d041b5654b6e513866062a095c85803d
sdshaoda <sdshaoda@126.com>
2024-04-15 20:00:07 +0800

    feat(rsc): client router

--- rsc/index.ts
+++ rsc/index.ts
@@ -1,12 +1,21 @@
 import express from 'express'
+import { readFile } from 'fs/promises'
 import { htmlGenerator } from './generator'
 const app = express()
 
 app.get('/:route(*)', async (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`)
-  const html = await htmlGenerator(url)
-  res.setHeader('Content-Type', 'text/html')
-  res.end(html)
+
+  // 匹配 client.js
+  if (url.pathname === '/client.js') {
+    const content = await readFile('./client.js', 'utf8')
+    res.setHeader('Content-Type', 'text/javascript')
+    res.end(content)
+  } else {
+    const html = await htmlGenerator(url)
+    res.setHeader('Content-Type', 'text/html')
+    res.end(html)
+  }
 })
 
 app.listen(3000, err => {
